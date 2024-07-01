10f49894550dda32e21b6a5624101acc1325142b
Gabriel Valle <gabrielvalle33210@gmail.com>
2024-06-30 22:11:51 -0700

    overlay

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-const CAT_API_URL = "https://api.thecatapi.com/v1/images/search";
-const DOG_API_URL = "https://api.thedogapi.com/v1/images/search";
+const CAT_API_URL = "https://api.thecatapi.com/v1/images/search?has_breeds=1";
+const DOG_API_URL = "https://api.thedogapi.com/v1/images/search?has_breeds=1";
 
 async function fetchImage(url) {
     try {
@@ -16,4 +16,67 @@ async function fetchImage(url) {
     }
 }
 
-console.log(fetchImage(CAT_API_URL));
+async function loadImages() {
+    const imageGrid = document.querySelector(".image-grid");
+
+    // Fetch cat images
+    const catImages = await Promise.all(
+        Array.from({ length: 5 }, () => {
+            return fetchImage(CAT_API_URL);
+        })
+    );
+    // Fetch dog images
+    const dogImages = await Promise.all(
+        Array.from({ length: 5 }, () => {
+            return fetchImage(DOG_API_URL);
+        })
+    );
+
+    // Combine arrays
+    const allImages = [...catImages, ...dogImages];
+    allImages.sort(() => Math.random() - 0.5);
+
+    const sizes = ["horizontal", "vertical", "square", ""];
+
+    allImages.forEach((images) => {
+        const imgElement = document.createElement("img");
+        imgElement.src = images[0].url;
+        
+        const pElement = document.createElement("p");
+        pElement.innerHTML = "test";
+
+        // console.log(images[0].breeds[0].name);
+
+        const overlayDiv = document.createElement("div");
+        overlayDiv.classList.add("image-overlay");
+        overlayDiv.appendChild(pElement);
+
+        const divElement = document.createElement("div");
+        divElement.classList.add("image-container");
+        divElement.appendChild(imgElement);
+        divElement.appendChild(overlayDiv);
+
+        // Randomly assign a size class
+        const randomSize = sizes[Math.floor(Math.random() * sizes.length)];
+        if (randomSize) {
+            divElement.classList.add(randomSize);
+        }
+
+        imageGrid.appendChild(divElement);
+    });
+}
+
+function isBottomOfPage() {
+    return window.innerHeight + window.scrollY >= document.body.offsetHeight - 100;
+}
+
+function handleScroll() {
+    if (isBottomOfPage()) {
+        loadImages();
+    }
+}
+
+window.addEventListener("scroll", handleScroll);
+
+// Initial load
+loadImages();
